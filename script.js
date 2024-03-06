1573f7b9bcc3391a62f2779b78a1784781fb6c05
“Harshada_Patil” <patilharshada1911@gmail.com>
2024-03-06 18:07:48 +0530

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-
-const button = document.getElementById("Button");
- button.addEventListener('click', async ()=> {
-    const randomUser =  await fetchdata();
-    displaydata(randomUser);
-
- });
-
-const url = "https://randomuser.me/api/";
-
-async function fetchdata()
-{
+const url = "https://api.thecatapi.com/v1/images/search";
+const img = document.getElementById("catImage");
+async function displayimage(){
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    return data.results[0];
+     img.src =data[0].url;
+
 }
-function displaydata(user){
-    if(user){
-        const {name, email, picture} = user;
-        const show = document.getElementById("user-container");
-        show.innerHTML = `
-        <img src="${picture.large}">
-        <p> Name: ${name.first} ${name.last}</p>
-        <p> Email : ${email}</p>
-        `;
-
-    }
-}
-
-
-
-
-
-  
+displayimage();
